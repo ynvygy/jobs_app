6649ff33327b631c1dbaddfdceb4da8be9f7996a
ynvygy <lugerand2004@yahoo.com>
2016-07-18 05:28:29 +0300

    added some view files

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,4 +1,24 @@
 class JobsController < ApplicationController
   def index
   end
+
+  def new
+  	@job = Job.new
+  end
+
+  def create
+  	@job = Job.new(job_params)
+  	@job.save
+  	redirect_to @job
+  end
+
+  def show
+  	@job = Job.find(params[:id])
+  end
+
+  private
+
+  def job_params
+  	params.require(:job).permit(:name, :description, :budget, :location)
+  end
 end
